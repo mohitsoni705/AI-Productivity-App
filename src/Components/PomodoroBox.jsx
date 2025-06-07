2e204fa8664ba5dd918a7faddc6b922812dad3ba
mohitsoni705 <mohitravet07@gmail.com>
2025-06-07 22:09:31 +0530

    added start and reset button in pomodoro

--- src/Components/PomodoroBox.jsx
+++ src/Components/PomodoroBox.jsx
@@ -1,14 +1,38 @@
-import React, { use, useState } from 'react'
+import React, { use, useEffect, useRef, useState } from 'react'
 
 const PomodoroBox = () => {
     const [pomodoroTime, setPomodoroTime] = useState(1500);
+    const [isRunning, setIsRunning] = useState(false);
+    const intervalRef = useRef(null);
     const formatTime = (pomodoroTime) => {
         const minutes = Math.floor(pomodoroTime / 60);
         const seconds = pomodoroTime % 60;
         return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
     const handleStartButton =()=>{ 
-    }
+      if(!isRunning && pomodoroTime > 0){
+        setIsRunning(true);
+
+      }
+      }
+      const handleResetButton =()=>{
+        setIsRunning(false);
+        setPomodoroTime(1500);
+      }
+      useEffect(() => {
+        if (isRunning && pomodoroTime > 0) {
+            intervalRef.current = setInterval(() => {
+                setPomodoroTime(prevTime => prevTime - 1);
+            }, 1000);
+        } else if (!isRunning) {
+            clearInterval(intervalRef.current);
+        }
+        if (pomodoroTime === 0) {
+            setIsRunning(false);
+            clearInterval(intervalRef.current);
+        }
+        return () => clearInterval(intervalRef.current);
+    }, [isRunning, pomodoroTime]);
   return (
     <div className='container-pomodoro'>
       <div>
@@ -25,7 +49,7 @@ const PomodoroBox = () => {
       <div><button>Edit</button></div>
       <div className='button-group'>
         <button className='start-button' onClick={handleStartButton}>Start</button>
-        <button className='reset-button'>Reset</button>
+        <button className='reset-button' onClick={handleResetButton}>Reset</button>
       </div>
     </div>
   )
